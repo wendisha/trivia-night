5276b952327c5f38d5c51068edf68fbfc3e489d0
Wendy Calderon <wendycalderon@gmail.com>
2020-05-09 16:10:49 -0400

    Hook buttons

--- src/TriviaContainer.jsx
+++ src/TriviaContainer.jsx
@@ -4,22 +4,35 @@ import TriviaLayout from "./TriviaLayout";
 class GameContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      OnePlayer: false,
+      TeamUp: false,
+    };
     this.handleClickOnePlayer = this.handleClickOnePlayer.bind(this);
     this.handleClickTeamUp = this.handleClickTeamUp.bind(this);
   }
 
   handleClickOnePlayer() {
-    console.log("Click happened");
+    console.log("Click happened for One Player");
+    this.setState({
+      OnePlayer: true,
+    });
   }
 
   handleClickTeamUp() {
-    console.log("Click happened");
+    console.log("Click happened for Team Up");
+    this.setState({
+      TeamUp: true,
+    });
   }
 
   render() {
     return (
       <div>
-        <TriviaLayout />
+        <TriviaLayout
+          handleClickOnePlayer={this.handleClickOnePlayer}
+          handleClickTeamUp={this.handleClickTeamUp}
+        />
       </div>
     );
   }
--- src/TriviaLayout.jsx
+++ src/TriviaLayout.jsx
@@ -1,28 +1,20 @@
 import React from "react";
-// import logo from "./logo.svg";
 import logo from "./tn6.jpg";
 import "./App.css";
 
-function TriviaLayout() {
+function TriviaLayout({ handleClickOnePlayer, handleClickTeamUp }) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
         <p className="App-title">Version 1.0: LA TERTULIA</p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         <div className="flex-container">
-          <button className="Pink-button">Individual</button>
-          <button className="Green-button">Equipos</button>
+          <button className="Pink-button" onClick={handleClickOnePlayer}>
+            Individual
+          </button>
+          <button className="Green-button" onClick={handleClickTeamUp}>
+            Equipos
+          </button>
         </div>
       </header>
     </div>
