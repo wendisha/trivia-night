6927d74555813c76909d0941fe1f77da44145a35
Wendy Calderon <wendycalderon@gmail.com>
2020-05-09 12:03:31 -0400

    Intro page ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import logo from "./tn6.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        <p className="App-title">Version 1.0: LA TERTULIA</p>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <button className="Button">Empezar</button>
       </header>
     </div>
   );
